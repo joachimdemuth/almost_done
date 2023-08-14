f1eca33adb78787001cf61d293e1010f2e6db34a
Joachim <jde@maybetomorrow.dk>
2023-08-14 22:04:27 +0200

    added meta tag

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,8 @@ export default function RootLayout({ children }) {
 					href='https://fonts.cdnfonts.com/css/tasa-orbiter-display'
 					rel='stylesheet'
 				></link>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+
 				<link
 					href='https://fonts.cdnfonts.com/css/tasa-orbiter-text'
 					rel='stylesheet'
