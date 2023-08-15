b512f64e551f9c612653046467faeccec35b2e9f
Joachim <jde@maybetomorrow.dk>
2023-08-15 15:20:53 +0200

    test done

--- app/page.js
+++ app/page.js
@@ -52,7 +52,7 @@ export default function Home() {
 					className='w-full min-h-screen object-cover'
 				/>
 			</div>
-			<div className='flex fixed top-0 left-0 min-w-full min-h-screen max-lg:pt-40 max-lg:items-start justify-center items-center lg:items-center z-10'>
+			<div className='flex fixed top-0 left-0 w-full min-h-screen max-lg:pt-40 max-lg:items-start justify-center items-center lg:items-center z-10'>
 				
 					<svg className='w-[80%] max-w-[800px] h-auto' overflow={'visible'} viewBox="0 0 56 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<defs>
@@ -102,7 +102,7 @@ export default function Home() {
 				</svg>
 				
 			</div>
-			<div className='z-10 flex fixed flex-col w-full lg:px-20 min-h-screen max-md:bottom-16  justify-end items-center pb-20'>
+			<div className='z-10 flex fixed flex-col w-full lg:px-20 min-h-screen bottom-16  justify-end items-center pb-20'>
 				<Link href='/main-hall'>
 					<div className='flex px-4 py-6  items-center gap-2 border-4 rounded-full  border-primary-lime-green hover:animate-[prolong_0.2s_ease-in-out_forwards]'>
 						<div className='flex items-center'>
